f90d08c1882976940a1cacfee53fcbc85e6f0193
Ackhapocha <prihodko@bmstu.ru>
2026-03-29 17:18:23 +0300

    Решение домашнего задания

---
+++ home-works/001/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func getResult(arr []int) string {
+	sorted, mean := calc(arr)
+	res := "выход:\n\"Отсортированные элементы: "
+	for i := 0; i < len(sorted); i++ {
+		res += fmt.Sprintf("%d ", sorted[i])
+	}
+	res = res[:len(res)-1]
+	return res + fmt.Sprintf("\nСамое большое число: %d\n", sorted[0]) +
+		fmt.Sprintf("Самое большое число: %d\n", sorted[4]) +
+		fmt.Sprintf("Среднее арифметическое: %d\n\"\n", mean)
+}
+
+func calc(arr []int) ([]int, int) {
+	items, summ := len(arr), 0
+	for i := 1; i < len(arr); i++ {
+		summ += arr[i]
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(arr)))
+	return arr, summ / items
+}
+
+func main() {
+	var a, b, c, d, e int
+	fmt.Printf("вход: ")
+	fmt.Scanln(&a, &b, &c, &d, &e)
+	arr := []int{a, b, c, d, e}
+	fmt.Println(getResult(arr))
+}
