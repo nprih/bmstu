0dfe7b95712620bc32b0321e8a650c433fdfa391
Ackhapocha <prihodko@bmstu.ru>
2026-04-02 21:01:39 +0300

    Классная работа. Системы счисления

--- lessons/004/main.go
+++ lessons/004/main.go
@@ -2,39 +2,9 @@ package main
 
 import "fmt"
 
-const costA = 12.5
-const costB = 8.3
-
-func input() (uint, uint) {
-	var a, b uint
-
-	fmt.Printf("Введите количество километров: ")
-	fmt.Scanln(&a)
-	fmt.Printf("Введите количество минут: ")
-	fmt.Scanln(&b)
-
-	return a, b
-}
-
-func calc(a, b uint) (float64, float64) {
-	sumA := float64(a) * costA
-	sumB := float64(b) * costB
-
-	return sumA, sumB
-}
-
-func printRes(sumA, sumB float64) {
-	fmt.Printf("\nЦена поездки в км: %.2f\n", sumA)
-	fmt.Printf("Цена поездки в минутах: %.2f\n", sumB)
-	res := ""
-	if sumA > sumB {
-		res = "по минутам"
-	} else {
-		res = "по километрам"
-	}
-	fmt.Printf("\nЛучше использовать тариф: %s\n", res)
-}
-
 func main() {
-	printRes(calc(input()))
+	var r, g, b uint8 = 0, 141, 213
+	//var r, g, b uint8 = 0x00, 0x8d, 0xd5
+	//fmt.Printf("color: #%02x%02x%02x\n", r16, g16, b16)
+	fmt.Printf("color: #%02x%02x%02x\n", r, g, b)
 }
