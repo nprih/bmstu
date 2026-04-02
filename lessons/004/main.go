ea5456f6e482461d9b9df4e07d0c48e69b2ed357
Ackhapocha <prihodko@bmstu.ru>
2026-04-02 20:47:29 +0300

    Классная работа. Задачка про каршеринг.

--- lessons/004/main.go
+++ lessons/004/main.go
@@ -2,16 +2,33 @@ package main
 
 import "fmt"
 
-func factorial(a uint) uint {
-	var b, i uint = 1, 1
-	for i <= a {
-		b *= i
-		i++
-	}
-	return b
+const costA = 12.5
+const costB = 8.3
+
+func calc(a, b uint) (float64, float64) {
+	sumA := float64(a) * costA
+	sumB := float64(b) * costB
+
+	return sumA, sumB
 }
+
 func main() {
-	var a uint
+	var a, b uint
+
+	fmt.Printf("Введите количество километров: ")
 	fmt.Scanln(&a)
-	fmt.Println(factorial(a))
+	fmt.Printf("Введите количество минут: ")
+	fmt.Scanln(&b)
+
+	sumA, sumB := calc(a, b)
+
+	fmt.Printf("\nЦена поездки в км: %.2f\n", sumA)
+	fmt.Printf("Цена поездки в минутах: %.2f\n", sumB)
+	res := ""
+	if sumA > sumB {
+		res = "по минутам"
+	} else {
+		res = "по километрам"
+	}
+	fmt.Printf("\nЛучше использовать тариф: %s\n", res)
 }
