f79f0aeff5cf4ee363de888339c0283fa3e95c71
Ackhapocha <prihodko@bmstu.ru>
2026-04-02 21:30:41 +0300

    Классная работа. Даты

--- lessons/004/main.go
+++ lessons/004/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	var r, g, b uint8 = 0, 141, 213
-	//var r, g, b uint8 = 0x00, 0x8d, 0xd5
-	//fmt.Printf("color: #%02x%02x%02x\n", r16, g16, b16)
-	fmt.Printf("color: #%02x%02x%02x\n", r, g, b)
+	// 01   02   03   04   05   06   Mon
+	// Jan 02.  15.   04.  05.  2006 Monday
+	// 2 ярваря 2006 15 часов: 04 минуты: 05 секунд
+
+	nowTime := time.Now()
+	fmt.Println(nowTime)
+	fmt.Println(nowTime.Format("2006 Jan 02 03:04:05 Monday"))
+	fmt.Println(nowTime.Format("02.01.06"))
+	fmt.Println(nowTime.Format(time.RFC3339))
+	fmt.Println(nowTime.Format(time.DateTime))
 }
