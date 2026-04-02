350a29b671fbd553740b15a276d41964b654e16b
Ackhapocha <prihodko@bmstu.ru>
2026-04-02 21:35:21 +0300

    Классная работа. Даты

--- lessons/004/main.go
+++ lessons/004/main.go
@@ -11,9 +11,12 @@ func main() {
 	// 2 ярваря 2006 15 часов: 04 минуты: 05 секунд
 
 	nowTime := time.Now()
-	fmt.Println(nowTime)
-	fmt.Println(nowTime.Format("2006 Jan 02 03:04:05 Monday"))
-	fmt.Println(nowTime.Format("02.01.06"))
-	fmt.Println(nowTime.Format(time.RFC3339))
-	fmt.Println(nowTime.Format(time.DateTime))
+	//fmt.Println(nowTime)
+	//fmt.Println(nowTime.Format("2006 Jan 02 03:04:05 Monday"))
+	//fmt.Println(nowTime.Format("02.01.06"))
+	//fmt.Println(nowTime.Format(time.RFC3339))
+	//fmt.Println(nowTime.Format(time.DateTime))
+	//fmt.Println(nowTime.Format("06.02.01 15:04"))
+	//fmt.Println(nowTime.Format("02 January 06"))
+	fmt.Println(nowTime.Format("15:04 Mon 2006/01/02"))
 }
