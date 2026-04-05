fd883ddee8f9743134b102a16da304239fb91cad
Ackhapocha <prihodko@bmstu.ru>
2026-04-05 11:42:32 +0300

    Классная работа. Строки

--- lessons/005/main.go
+++ lessons/005/main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var pi rune = 960
-	var alpha rune = 940
-	var omega rune = 969
-	fmt.Printf("Ur symbols: %c, %c, %c\n", pi, alpha, omega)
-	// %s %d %c %v
+	peace := "Hello, world"
+	for i := 0; i < len(peace); i++ {
+		fmt.Printf("%c\n", peace[i])
+	}
 }
