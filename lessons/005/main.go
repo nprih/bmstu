ba7134d5cfc5b47d4e4c953f1eda9a32470c04b2
Ackhapocha <prihodko@bmstu.ru>
2026-04-05 12:20:38 +0300

    Классная работа. Строки. Шифровка.

--- lessons/005/main.go
+++ lessons/005/main.go
@@ -1,10 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func decrypt(code string) {
+	var c byte
+	deCode := ""
+	for i := 0; i < len(code); i++ {
+		c = code[i]
+		if c >= 'a' && c <= 'z' {
+			c = c - 3
+			if c < 'a' {
+				c = c + 26
+			}
+		}
+		deCode += string(c)
+	}
+	fmt.Println(deCode)
+}
 
 func main() {
-	peace := "Hello, world"
+	peace := "execute, my code"
+	var c byte
+	code := ""
+
 	for i := 0; i < len(peace); i++ {
-		fmt.Printf("%c\n", peace[i])
+		c = peace[i]
+		if c >= 'a' && c <= 'z' {
+			c = c + 3
+			if c > 'z' {
+				c = c - 26
+			}
+		}
+		code += string(c)
 	}
+
+	fmt.Println("\nТеперь дешифруем:")
+
+	decrypt(code)
 }
