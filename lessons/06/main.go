d04602832cfed6467bd54669cbeac09b741d45ab
Ackhapocha <prihodko@bmstu.ru>
2026-04-07 21:37:10 +0300

    Классная работа. Reader. Задачка на проверку четности.

--- lessons/06/main.go
+++ lessons/06/main.go
@@ -4,16 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("New line: ")
-	text, err := reader.ReadString('\n')
+func calc(text string) {
+	i, err := strconv.Atoi(strings.TrimSpace(text))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Вы ввели не число")
 		return
 	}
-	fmt.Println("Ur text: ", strings.TrimSpace(text))
+
+	res := i % 2
+	response := ""
+	if res == 0 {
+		response = "четное"
+	} else {
+		response = "не четное"
+	}
+
+	fmt.Println("Ваше число:", strings.TrimSpace(response))
+
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("New line: ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		calc(text)
+	}
 }
