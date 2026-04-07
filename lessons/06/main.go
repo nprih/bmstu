b07c42f974c639b08f8a0870f015d7216ce25b0a
Ackhapocha <prihodko@bmstu.ru>
2026-04-07 20:57:23 +0300

    Классная работа. Преобразование bool.

---
+++ lessons/06/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//ParceFloat / FormatFloat
+
+func main() {
+	var a string = "yes"
+	var readyForLaunch bool
+	readyForLaunch = (a == "yes")
+	fmt.Println("Ready fo space?", "-", readyForLaunch)
+
+	//var trueFalse string
+	//
+	//if a {
+	//	trueFalse = "true"
+	//} else {
+	//	trueFalse = "false"
+	//}
+	//println(trueFalse)
+}
