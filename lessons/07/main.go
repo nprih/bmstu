14037bbcb4e79150b97e8bd2f558d5da592a844c
Ackhapocha <prihodko@bmstu.ru>
2026-04-09 19:12:33 +0300

    Классная работа. Массивы.

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+// пустые строки
+
 func main() {
+	var planets [8]string
+	planets[0] = "Меркурий"
+	planets[1] = "Венера"
+	planets[2] = "Земля"
+
+	mercury := planets[0]
+	earth := planets[2]
+
+	fmt.Println(mercury, earth, planets[1])
+	fmt.Println(len(planets))
+	fmt.Println(planets[6] == "")
 
 }
