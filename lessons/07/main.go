1890004019bc714b0e4bab83ef7caba22cd78a4c
Ackhapocha <prihodko@bmstu.ru>
2026-04-09 21:08:41 +0300

    Классная работа. Slice. Cap

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -6,9 +6,9 @@ import "fmt"
 // Слайс - не фиксированная, по умолчанию, длина. Можно увеличивать...
 
 func main() {
-	planets_2 := []int{}
+	planets_2 := make([]int, 0, 10)
 	for i := 0; i < 30; i++ {
 		planets_2 = append(planets_2, i)
+		fmt.Printf("len slice: %v,  cap slice: %v\n", len(planets_2), cap(planets_2))
 	}
-	fmt.Println(cap(planets_2))
 }
