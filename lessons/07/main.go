d3c69f8a895e56656a0d87afa4b456aa05510e77
Ackhapocha <prihodko@bmstu.ru>
2026-04-09 19:17:52 +0300

    Классная работа. Массивы. Композитный литерал.

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -5,16 +5,10 @@ import "fmt"
 // пустые строки
 
 func main() {
-	var planets [8]string
-	planets[0] = "Меркурий"
-	planets[1] = "Венера"
-	planets[2] = "Земля"
-
-	mercury := planets[0]
-	earth := planets[2]
-
-	fmt.Println(mercury, earth, planets[1])
-	fmt.Println(len(planets))
-	fmt.Println(planets[6] == "")
-
+	planets := [8]string{
+		"Меркурий",
+		"Венера",
+		"Земля",
+	}
+	fmt.Println(planets)
 }
