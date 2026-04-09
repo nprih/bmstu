753ca0e459b2c8792b33a15d5e3a94bb389e1922
Ackhapocha <prihodko@bmstu.ru>
2026-04-09 19:54:02 +0300

    Классная работа. Массивы.

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -4,18 +4,19 @@ import "fmt"
 
 // Массив - это всегда фиксированная длинна
 
+func change01index(arr [3]string) {
+	arr[0] = "different planet"
+}
+
 func main() {
 	planets := [...]string{
 		"Меркурий",
 		"Венера",
 		"Земля",
-		"Марс",
-		"Сатурн",
-		"Уран класная планета",
 	}
-	for i, _ := range planets {
-		fmt.Println(planets[i])
-		planets[i] = "Bad planet"
-	}
+
+	change01index(planets)
+
 	fmt.Println(planets)
+
 }
