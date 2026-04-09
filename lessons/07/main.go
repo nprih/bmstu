9a16acaada4f0af83a70579009deefe59b4b0943
Ackhapocha <prihodko@bmstu.ru>
2026-04-09 21:03:29 +0300

    Классная работа. Slice. Cap

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -6,9 +6,9 @@ import "fmt"
 // Слайс - не фиксированная, по умолчанию, длина. Можно увеличивать...
 
 func main() {
-	planets_2 := []string{"Москва", "Сочи", "Лондон", "Череповец"}
-	fmt.Println(len(planets_2))
-	planets_2 = append(planets_2[:2], planets_2[3:]...)
-	fmt.Println(planets_2)
-	fmt.Println(len(planets_2))
+	planets_2 := []int{}
+	for i := 0; i < 30; i++ {
+		planets_2 = append(planets_2, i)
+	}
+	fmt.Println(cap(planets_2))
 }
