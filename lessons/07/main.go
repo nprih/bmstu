df1bbb1bd7cfc0004f180ef68e00c11188613e5b
Ackhapocha <prihodko@bmstu.ru>
2026-04-09 21:37:23 +0300

    Классная работа. Slice. Cap

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -5,10 +5,18 @@ import "fmt"
 // Массив - это всегда фиксированная длина
 // Слайс - не фиксированная, по умолчанию, длина. Можно увеличивать...
 
+func changeZeroIndex(arr []int) {
+	arr[0] = 99999
+}
+
 func main() {
-	planets_2 := make([]int, 0, 10)
+	planets_2 := make([]int, 0, 100)
 	for i := 0; i < 30; i++ {
 		planets_2 = append(planets_2, i)
-		fmt.Printf("len slice: %v,  cap slice: %v\n", len(planets_2), cap(planets_2))
 	}
+	changeZeroIndex(planets_2)
+
+	planets_2 = append(planets_2[:1], planets_2[len(planets_2)-1])
+
+	fmt.Println(planets_2)
 }
