5c93c56b6aeee7e8462ae7e578ce10d50a6440ed
Ackhapocha <prihodko@bmstu.ru>
2026-04-12 13:04:22 +0300

    Классная работа. Defer.

--- lessons/08/main.go
+++ lessons/08/main.go
@@ -2,16 +2,10 @@ package main
 
 import "fmt"
 
-func WorkingWithDB() {
-	//Open DB no logic
-	//Some logic
-	//Close DB connection
-}
-
 func main() {
 	defer fmt.Println("This is defer #1")
 	defer fmt.Println("This is defer #2")
 	defer fmt.Println("This is defer #3")
-
+	panic("dssddsdsds")
 	fmt.Println("main function")
 }
