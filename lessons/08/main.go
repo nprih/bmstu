4e57bb6e7d86a26e433e6d33dc2e4a7bd8b37abc
Ackhapocha <prihodko@bmstu.ru>
2026-04-12 12:08:45 +0300

    Классная работа. Переменное количество параметров.

--- lessons/08/main.go
+++ lessons/08/main.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-func Sum(x ...int) int {
-	res := 0
+func Sum(x ...int) (res, zero int) {
 	for _, v := range x {
 		res += v
 	}
-	return res
+	return res, zero
 }
 
 func main() {
