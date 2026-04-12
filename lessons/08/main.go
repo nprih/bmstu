7d7b949bac8f75b97394a0eccbfbb70e1f083dc6
Ackhapocha <prihodko@bmstu.ru>
2026-04-12 12:26:59 +0300

    Классная работа. Передача функций в качестве параметра.

--- lessons/08/main.go
+++ lessons/08/main.go
@@ -2,21 +2,10 @@ package main
 
 import "fmt"
 
-func Say(animal string) string {
-	if animal == "cat" {
-		return "Meuw!"
-	} else if animal == "dog" {
-		return "Wuf!"
-	}
-	return "I dont know!"
-}
-
 func AnimalTalk(who string, how func(string) string) {
 	fmt.Println(how(who))
 }
 
 func main() {
-	var voice func(string) string
-	voice = Say
-	AnimalTalk("cat", voice)
+	AnimalTalk("dog", func(animal string) string { return "Wuf!" })
 }
