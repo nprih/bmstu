bcf9e8b47274e6760cfd8665397d4312749ce4e0
Ackhapocha <prihodko@bmstu.ru>
2026-04-12 11:04:12 +0300

    Классная работа. Ассоциативные массивы. Удаление элемента.

--- lessons/07/main.go
+++ lessons/07/main.go
@@ -5,12 +5,12 @@ import (
 )
 
 func main() {
-	//dmitry := make(map[string]string)
 	dmitry := map[string]string{
 		"Surname": "Verevkin",
 		"Name":    "Dmitry",
 		"Email":   "veryovkin@bmstu.ru",
 	}
+	delete(dmitry, "Name")
 	for k, v := range dmitry {
 		fmt.Println(k, ":", v)
 	}
