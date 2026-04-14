36217cd4a7cf0cbfd2a5f71a6020d9594389fd42
Ackhapocha <prihodko@bmstu.ru>
2026-04-14 19:54:47 +0300

    Классная работа. ООП. Создание своих типов данных. Метод.

--- lessons/09/main.go
+++ lessons/09/main.go
@@ -4,19 +4,22 @@ import "fmt"
 
 type celsius float64
 type kelvin float64
+type farenheit float64
 
-//func kelvinToCelsius(t kelvin) celsius {
-//	return celsius(t - 273.15)
-//}
+func (t farenheit) celsius() celsius {
+	//Без логики
+	return 100
+}
 
-func (t kelvin) сelsius() celsius {
+func (t kelvin) celsius() celsius {
+	t -= 100
 	return celsius(t - 273.15)
 }
 
 func main() {
-
 	var temperatureInCelsius celsius = 10.7
 	var temperatureInKelvin kelvin = 400
-
-	fmt.Println(temperatureInCelsius + temperatureInKelvin.сelsius())
+	var temperatureInFahrenheit farenheit = 10
+	fmt.Println(temperatureInCelsius + temperatureInKelvin.celsius())
+	fmt.Println(temperatureInCelsius + temperatureInFahrenheit.celsius())
 }
