e5eebe95ecd9e244967f7665e24de96e4f46b0de
Ackhapocha <prihodko@bmstu.ru>
2026-04-14 20:56:29 +0300

    Классная работа. ООП. Создание своих типов данных. Метод.

--- lessons/09/main.go
+++ lessons/09/main.go
@@ -8,11 +8,14 @@ type User struct {
 	Age   int
 }
 
+func (u User) sendMessage(message string) {
+	fmt.Printf("Message: %s was sent to %s \n", message, u.Email)
+}
 func main() {
-	var Ivan User = User{Name: "Ivan", Email: "Ivan@email.ru", Age: 19}
-	var Kirill User = User{Name: "Kirill", Email: "Kirill@email.ru", Age: 20}
-
+	var Ivan User = User{Name: "Ivan", Email: "Ivan@mail.ru", Age: 19}
+	var Kirill User = User{Name: "Kirill", Email: "Kirill@mail.ru", Age: 20}
 	users := []User{Ivan, Kirill}
-
-	fmt.Println(users)
+	for _, v := range users {
+		v.sendMessage("Welcome to afterparty!")
+	}
 }
