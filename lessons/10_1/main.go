494d33847fd627d403f9bd021b743f77ed276914
Ackhapocha <prihodko@bmstu.ru>
2026-04-16 21:43:06 +0300

    Классная работа. Интерфейсы.

--- lessons/10_1/main.go
+++ lessons/10_1/main.go
@@ -2,12 +2,53 @@ package main
 
 import "fmt"
 
-type Cat struct {
-	Name  string
-	Voice string
-}
+//type addressPrinter interface {
+//	talk()
+//	printAddr() string
+//}
+//type Cat struct {
+//	Name  string
+//	Voice string
+//}
+//
+//type Dog struct {
+//	Name  string
+//	Voice string
+//}
+//
+//func (c Cat) talk() {
+//	fmt.Printf("Cat tolk %s\n", c.Voice)
+//}
+//
+//func (c Dog) talk() {
+//	fmt.Printf("Dog tolk %s\n", c.Voice)
+//}
+//
+//func (c Cat) printAddr() string {
+//	return c.Name + "My cat"
+//}
+//
+//func (c Dog) printAddr() string {
+//	return c.Name + "My dog"
+//}
+//
+//func PrintAddressInConsole(animal addressPrinter) {
+//	fmt.Println(animal.printAddr)
+//}
+//
+//func main() {
+//	Barsik := Cat{Name: "Barsik", Voice: "Meuw"}
+//	Sharik := Dog{Name: "Sharik", Voice: "Wuf!"}
+//
+//	PrintAddressInConsole(Barsik)
+//	PrintAddressInConsole(Sharik)
+//}
 
-type Dog struct {
+type addressPrinter interface {
+	talk()
+	printAddr() string
+}
+type Cat struct {
 	Name  string
 	Voice string
 }
@@ -15,16 +56,28 @@ type Dog struct {
 func (c Cat) talk() {
 	fmt.Printf("Cat tolk %s\n", c.Voice)
 }
+func (c Cat) printAddr() string {
+	return c.Name + " myCat"
+}
 
-func (c Dog) talk() {
-	fmt.Printf("Dog tolk %s\n", c.Voice)
+type Dog struct {
+	Name  string
+	Voice string
+}
+
+func (d Dog) talk() {
+	fmt.Printf("Dog tolk %s\n", d.Voice)
+}
+func (d Dog) printAddr() string {
+	return d.Name + " myDog"
+}
+func PrintAddressInConsole(animal addressPrinter) {
+	fmt.Println(animal.printAddr())
 }
 
 func main() {
-	Barsik := Cat{Name: "Barsik", Voice: "Meuw"}
-	Sharik := Dog{Name: "Sharik", Voice: "Wuf!"}
-
-	Barsik.talk()
-	Sharik.talk()
-
+	Barsik := Cat{Name: "Barsic", Voice: "Muew!"}
+	Sharik := Dog{Name: "Sahrik", Voice: "Wuf!"}
+	PrintAddressInConsole(Barsik)
+	PrintAddressInConsole(Sharik)
 }
