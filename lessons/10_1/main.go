9b83eca676b7d51f2b45c2e2cf04d799dee8f536
Ackhapocha <prihodko@bmstu.ru>
2026-04-20 18:51:13 +0300

    Классная работа. Интерфейсы.

--- lessons/10_1/main.go
+++ lessons/10_1/main.go
@@ -2,52 +2,8 @@ package main
 
 import "fmt"
 
-//type addressPrinter interface {
-//	talk()
-//	printAddr() string
-//}
-//type Cat struct {
-//	Name  string
-//	Voice string
-//}
-//
-//type Dog struct {
-//	Name  string
-//	Voice string
-//}
-//
-//func (c Cat) talk() {
-//	fmt.Printf("Cat tolk %s\n", c.Voice)
-//}
-//
-//func (c Dog) talk() {
-//	fmt.Printf("Dog tolk %s\n", c.Voice)
-//}
-//
-//func (c Cat) printAddr() string {
-//	return c.Name + "My cat"
-//}
-//
-//func (c Dog) printAddr() string {
-//	return c.Name + "My dog"
-//}
-//
-//func PrintAddressInConsole(animal addressPrinter) {
-//	fmt.Println(animal.printAddr)
-//}
-//
-//func main() {
-//	Barsik := Cat{Name: "Barsik", Voice: "Meuw"}
-//	Sharik := Dog{Name: "Sharik", Voice: "Wuf!"}
-//
-//	PrintAddressInConsole(Barsik)
-//	PrintAddressInConsole(Sharik)
-//}
+type addressPrinter interface{} //any
 
-type addressPrinter interface {
-	talk()
-	printAddr() string
-}
 type Cat struct {
 	Name  string
 	Voice string
@@ -56,6 +12,7 @@ type Cat struct {
 func (c Cat) talk() {
 	fmt.Printf("Cat tolk %s\n", c.Voice)
 }
+
 func (c Cat) printAddr() string {
 	return c.Name + " myCat"
 }
@@ -72,7 +29,7 @@ func (d Dog) printAddr() string {
 	return d.Name + " myDog"
 }
 func PrintAddressInConsole(animal addressPrinter) {
-	fmt.Println(animal.printAddr())
+	fmt.Println(animal)
 }
 
 func main() {
