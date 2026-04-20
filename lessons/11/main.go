8f20282e7d900c25582cc7bbfca2eea567baf533
Ackhapocha <prihodko@bmstu.ru>
2026-04-20 21:04:35 +0300

    Классная работа. Горутины.

--- lessons/11/main.go
+++ lessons/11/main.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
-func sleepyGopher(index int, c chan int) {
-	time.Sleep(3 * time.Second)
-	c <- index
+func providerGopher(ch12 chan string) {
+	data := []string{"apple", "pen", "pineapple", "bad", "bad apple", "hello world"}
+	for _, v := range data {
+		time.Sleep(1 * time.Second)
+		ch12 <- v
+	}
+	ch12 <- ""
+}
+
+func analyzeGopher(ch12, ch23 chan string) {
+	for {
+		item := <-ch12
+		if item == "" {
+			ch23 <- ""
+			return
+		}
+		if !strings.Contains(item, "bad") {
+			ch23 <- item
+		}
+	}
+}
+
+func printerGopher(ch23 chan string) {
+	for {
+		item := <-ch23
+		if item == "" {
+			return
+		}
+		fmt.Printf("%s, ", item)
+	}
 }
 
 func main() {
-	c := make(chan int)
+	ch12 := make(chan string)
+	ch23 := make(chan string)
 
-	for i := 0; i < 5; i++ {
-		go sleepyGopher(i, c)
-	}
-	for i := 0; i < 5; i++ {
-		gopherId := <-c
-		fmt.Printf("Gopher %d finished work\n", gopherId)
-	}
+	go providerGopher(ch12)
+	go analyzeGopher(ch12, ch23)
+
+	printerGopher(ch23)
 }
