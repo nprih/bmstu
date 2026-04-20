657047d5067a1fecaa23d1f6c93e5b1f3c04bd28
Ackhapocha <prihodko@bmstu.ru>
2026-04-20 19:25:46 +0300

    Классная работа. Горутины.

--- lessons/11/main.go
+++ lessons/11/main.go
@@ -5,12 +5,14 @@ import (
 	"time"
 )
 
-func sleepyGopher() {
+func sleepyGopher(index int) {
 	time.Sleep(3 * time.Second)
-	fmt.Println("hrrr...")
+	fmt.Println("hrrr... #", index)
 }
 
 func main() {
-	go sleepyGopher()
+	for i := 0; i < 5; i++ {
+		go sleepyGopher(i)
+	}
 	time.Sleep(4 * time.Second)
 }
