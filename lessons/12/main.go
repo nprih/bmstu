89affcda80153e03a82bac8e7144287c929aaf60
Ackhapocha <prihodko@bmstu.ru>
2026-04-21 20:30:14 +0300

    Классная работа. Горутины. WaitGroup.

--- lessons/12/main.go
+++ lessons/12/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
+func sleepyGopher(index int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+	fmt.Println(index)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 7; i++ {
+		wg.Add(1)
+		go sleepyGopher(i, &wg)
+	}
+	wg.Wait()
 }
