abe2b69de9412cae86ce714f73b703044d36db83
Ackhapocha <prihodko@bmstu.ru>
2026-04-23 00:06:03 +0300

    Решение домашнего задания 1.7. Задача №2 решена.

--- home-works/1_7/02.go
+++ home-works/1_7/02.go
@@ -5,114 +5,73 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
-const quit = "exit"
-
-var cities []string
-var stop = ""
-var newCity string
-var search string
-
+var cities = []string{"Москва", "Балашиха", "Можайск", "Александров", "Одинцово",
+	"Мытищи", "Реутов", "Владимир", "Лобня", "Дмитров"}
+var line = strings.Repeat("-", 100)
+var inpCity string
 var err error
-var setText = "Исходный слайс с городами: "
-var addText = "Введенный город добавлен:  "
-var delText = "Введенный город удален:    "
 
 func main() {
-	cities = []string{
-		"Москва",
-		"Санкт-Петербург",
-		"Можайск",
-		"Александров",
-		"Одинцово",
-		"Мытищи",
-		"Реутов",
-		"Владимир",
-		"Лобня",
-		"Дмитров",
-	}
-	fmt.Printf("%s%v\n\n", setText, cities)
-
+	fmt.Printf("%s\n\n%s\n\n%s\n", line, "Список городов: ",
+		getCitiesColumn(false, 0))
 	addCity()
-
-	if stop == quit {
-		return
-	}
-
 	deleteCity()
-
-	if stop == quit {
-		return
-	}
-
-	//findCity()
-
+	findCity()
 }
 
 func addCity() {
-	inputAddCity()
-	if stop == quit {
-		return
-	}
-	cities = append(cities, strings.TrimSpace(newCity))
-	fmt.Printf("%s%v\n\n", addText, cities)
+	inputCity("Ведите город, который хотите добавить: ")
+	cities = append(cities, strings.TrimSpace(inpCity))
+	fmt.Printf("\n%s\n\nГород \"%s\" добавлен:\n\n%s\n", line, inpCity,
+		getCitiesColumn(false, 0))
 }
 
 func deleteCity() {
-	inputDeleteCity()
-	if stop == quit {
-		return
-	}
+	inputCity("Ведите город, который хотите удалить: ")
 	exist, index := find()
-
 	if exist {
 		cities = append(cities[:index], cities[index+1:]...)
-		fmt.Printf("%s%v\n\n", delText, cities)
-	}
-
-}
-
-func inputAddCity() {
-	stop = ""
-	fmt.Print("Ведите город, который хотите добавить: ")
-	reader := bufio.NewReader(os.Stdin)
-	newCity, err = reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		stop = quit
-		return
-	}
-}
-
-func inputDeleteCity() {
-	stop = ""
-	fmt.Print("Ведите город, который хотите удалить: ")
-	reader := bufio.NewReader(os.Stdin)
-	search, err = reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		stop = quit
-		return
+		fmt.Printf("\n%s\n\nВведенный город (\"%s\") удален: \n\n%s\n", line, inpCity,
+			getCitiesColumn(false, 0))
+	} else {
+		fmt.Printf("\n%s\n\nГород \"%s\" не найден в списке\n\n%s\n\n", line, inpCity, line)
 	}
 }
 
 func findCity() {
-	inputFindCity()
-
+	inputCity("Ведите город, который хотите найти: ")
 	exist, index := find()
 	if exist {
-		fmt.Println(index)
+		fmt.Printf("\n%s\n\nВыбранный город (\"%s\") выделен в списке:\n\n%s\n", line, inpCity,
+			getCitiesColumn(exist, index))
+	} else {
+		fmt.Printf("\n%s\n\nГород \"%s\" не найден в списке\n\n%s\n\n", line, inpCity, line)
 	}
 }
-func inputFindCity() {
-	fmt.Print("Ведите город, который хотите найти: ")
+
+func inputCity(message string) {
+	fmt.Print(message)
 	reader := bufio.NewReader(os.Stdin)
-	search, err = reader.ReadString('\n')
+	inpCity, err = reader.ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	capitalizeWords()
+}
+
+func getCitiesColumn(selected bool, index int) (res string) {
+	for i, city := range cities {
+		if selected && index == i {
+			res += "~ " + city + " ~" + "\n"
+		} else {
+			res += city + "\n"
+		}
+	}
+	return res
 }
 
 func find() (bool, int) {
@@ -120,8 +79,21 @@ func find() (bool, int) {
 	for i, city := range cities {
 		mapCities[city] = i
 	}
-	if index, exist := mapCities[strings.TrimSpace(newCity)]; exist {
+	if index, exist := mapCities[inpCity]; exist {
 		return true, index
 	}
 	return false, 0
+}
+
+func capitalizeWords() {
+	inpCity = strings.TrimSpace(inpCity)
+	char := []rune(inpCity)
+	for i := range char {
+		if i == 0 {
+			char[i] = unicode.ToUpper(char[i])
+		} else {
+			char[i] = unicode.ToLower(char[i])
+		}
+	}
+	inpCity = string(char)
 }
