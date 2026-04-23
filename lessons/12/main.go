fe643069ad53347e74a6fee9bdf30765dc9e5c12
Ackhapocha <prihodko@bmstu.ru>
2026-04-23 19:14:19 +0300

    Классная работа. Потоки и файлы.

--- lessons/12/main.go
+++ lessons/12/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Enter your name: ")
+	//input, err := reader.ReadString('\n')
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println(input)
+
+	fmt.Fprintln(os.Stdout, "Hello", "World")
 }
