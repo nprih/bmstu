ce6b56b6efd4280e7a999a8bc36a279cc4e6179e
Ackhapocha <prihodko@bmstu.ru>
2026-04-26 00:36:52 +0300

    Решение домашнего задания 1.10. Задача №1 решена.

--- home-works/1_10/01.go
+++ home-works/1_10/01.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 const course = 1 / 5828215.00 //на 26.04.2026
+const negativeMoney = "Ошибка: Сумма платежа должна быть положительной"
+const notEnoughMoney = "Ошибка: У Вас не достаточно средств."
 
 type PaymentProcessor interface {
 	GetParams() map[string]string
@@ -25,7 +27,10 @@ func (account *CreditCard) GetParams() map[string]string {
 
 func (account *CreditCard) Process(amount float64) string {
 	if amount <= 0 {
-		return fmt.Sprintf("Ошибка: Сумма платежа должна быть положительной")
+		return fmt.Sprintf(negativeMoney)
+	}
+	if account.Balance < amount {
+		return fmt.Sprintf(notEnoughMoney)
 	}
 	account.Balance -= amount
 	return fmt.Sprintf("Оплата картой #%s на сумму %.2f руб. успешно обработана. Баланс карты: %.2f",
@@ -49,9 +54,12 @@ func (account *CryptoWallet) GetParams() map[string]string {
 
 func (account *CryptoWallet) Process(amount float64) string {
 	if amount <= 0 {
-		return fmt.Sprintf("Ошибка: Сумма платежа быть положительной")
+		return fmt.Sprintf(negativeMoney)
 	}
 	cryptAmount := amount * course
+	if account.Balance < cryptAmount {
+		return fmt.Sprintf(notEnoughMoney)
+	}
 	account.Balance -= cryptAmount
 	return fmt.Sprintf("Оплата кошельком #%s на сумму %.2f RUB. (%.e %s) успешно обработана. Баланс: %e",
 		account.WalletId, amount, cryptAmount, account.Currency, account.Balance)
@@ -62,6 +70,18 @@ var accounts = []PaymentProcessor{
 		PaySystem:  "Банковская карта",
 		CardNumber: "1111 2222 3333 4444",
 		Balance:    5000,
+		Currency:   "RUB",
+	},
+	&CryptoWallet{
+		PaySystem: "Виртуальный кошелек",
+		WalletId:  "987654321987654321",
+		Balance:   20000.00 * course,
+		Currency:  "BTC",
+	},
+	&CreditCard{
+		PaySystem:  "Банковская карта",
+		CardNumber: "2222 3333 4444 5555",
+		Balance:    10000,
 		Currency:   "RUB",
 	},
 	&CryptoWallet{
@@ -80,7 +100,7 @@ func runApp() {
 	for _, processor := range accounts {
 		fmt.Printf("%s #%s:\n%s\n\n", processor.GetParams()["system"],
 			processor.GetParams()["id"], processor.GetParams()["balance"])
-		amounts := []float64{500.00, -500.00, 1200.00, 75.50}
+		amounts := []float64{500.00, -500.00, 1200.00, 75.50, 5000.00}
 		for _, amount := range amounts {
 			fmt.Printf("Платёж %.2f RUB: %s\n", amount, processor.Process(amount))
 		}
