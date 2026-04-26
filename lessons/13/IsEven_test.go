4e67c17a1af3493937a1c3c6d3ab050821f3ea8f
Ackhapocha <prihodko@bmstu.ru>
2026-04-26 12:30:21 +0300

    Классная работа. Тестирование. Benchmark.

--- lessons/13/IsEven_test.go
+++ lessons/13/IsEven_test.go
@@ -2,8 +2,20 @@ package main
 
 import "testing"
 
-func BenchmarkSimple(b *testing.B) {
+func BenchmarkInsertXInMap100000(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		//without logic
+		InsertXInMap(100000, b)
+	}
+}
+
+func BenchmarkInsertXInMap1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		InsertXInMap(1000, b)
+	}
+}
+
+func BenchmarkInsertXInMap100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		InsertXInMap(100, b)
 	}
 }
--- lessons/13/main.go
+++ lessons/13/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "testing"
+
+func InsertXInMap(x int, b *testing.B) {
+	testMap := make(map[int]int, 0)
+	b.ResetTimer()
+	for i := 0; i < x; i++ {
+		testMap[i] = i
+	}
+}
