786aceaa88d6f0222fa2e9ceeca818ae45ac0eff
Ackhapocha <prihodko@bmstu.ru>
2026-04-26 10:43:58 +0300

    Классная работа. Тестирование.

---
+++ lessons/13/IsEven_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestIsEven(t *testing.T) {
+	result1 := IsEven(2)
+	if result1 != "Yes" {
+		t.Errorf("Incorrect resilt, got: %s, but want: %s", result1, "Yes")
+	}
+	t.Log("Test with param 2 ok")
+
+	result2 := IsEven(3)
+	if result2 != "No" {
+		t.Errorf("Incorrect resilt, got: %s, but want: %s", result2, "No")
+	}
+	t.Log("Test with param 3 fail")
+}
---
+++ lessons/13/main.go
@@ -0,0 +1,9 @@
+package main
+
+func IsEven(input int) string {
+	if input%2 == 0 {
+		return "Yes"
+	} else {
+		return "No"
+	}
+}
