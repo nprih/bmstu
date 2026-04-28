43397dca766d3f6c0b0f10d97ccb6c9c3b62a8da
Ackhapocha <prihodko@bmstu.ru>
2026-04-28 17:15:48 +0300

    Классная работа. Тестирование. Benchmark. Slice.

--- lessons/13/IsEven_test.go
+++ lessons/13/IsEven_test.go
@@ -35,3 +35,37 @@ func BenchmarkInsertXInSlice100(b *testing.B) {
 		InsertXInSlice(100, b)
 	}
 }
+
+func BenchmarkSelectXFromMap100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SelectXFromMap(100000, b)
+	}
+}
+
+func BenchmarkSelectXFromMap1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SelectXFromMap(1000, b)
+	}
+}
+
+func BenchmarkSelectXFromMap100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SelectXFromMap(100, b)
+	}
+}
+
+func BenchmarkSelectXFromSlice100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SelectXFromSlice(100000, b)
+	}
+}
+func BenchmarkSelectXFromSlice1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SelectXFromSlice(1000, b)
+	}
+}
+func BenchmarkSelectXFromSlice100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SelectXFromSlice(100, b)
+	}
+}
--- lessons/13/main.go
+++ lessons/13/main.go
@@ -17,3 +17,31 @@ func InsertXInSlice(x int, b *testing.B) {
 		testSlice = append(testSlice, i)
 	}
 }
+
+func SelectXFromMap(x int, b *testing.B) {
+	testMap := make(map[int]int, x)
+	for i := 0; i < x; i++ {
+		testMap[i] = i
+	}
+	var h int
+	b.ResetTimer()
+	for i := 0; i < x; i++ {
+		h = testMap[i]
+	}
+	if h != x {
+	}
+}
+
+func SelectXFromSlice(x int, b *testing.B) {
+	testSlice := make([]int, x)
+	for i := 0; i < x; i++ {
+		testSlice[i] = i
+	}
+	var h int
+	b.ResetTimer()
+	for i := 0; i < x; i++ {
+		h = testSlice[i]
+	}
+	if h != x {
+	}
+}
