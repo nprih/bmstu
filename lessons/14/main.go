672c1386dc7df24b853cddf0d87e5a6dd9380cbe
Ackhapocha <prihodko@bmstu.ru>
2026-04-30 20:53:24 +0300

    Классная работа. HTML/CSS. Сгенерированная страница. Запуск сервера.

---
+++ lessons/14/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is main page")
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is about page")
+}
+func contactsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is contacts page")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contacts", contactsHandler)
+	log.Println("Server starting...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
+}
