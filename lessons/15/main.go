ef741c90837a67a61cf998e24c440b53ffe46774
Ackhapocha <prihodko@bmstu.ru>
2026-05-05 21:37:03 +0300

    Классная работа. Ошибки.

--- lessons/15/main.go
+++ lessons/15/main.go
@@ -1,28 +1,17 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-type MyError struct {
-	Code int
-	Text string
-}
-
-func (e *MyError) Error() string {
-	return e.Text
-}
-
-func doSomething() error {
-	return &MyError{Code: 404, Text: "Not found"}
+func badFunc() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("It was panic, but i'm stronger", r)
+			fmt.Println("panic was: ", r)
+		}
+	}()
+	panic("I'm bad function")
 }
 
 func main() {
-	err := doSomething()
-	var customError *MyError
-	if errors.As(err, &customError) {
-		fmt.Println(customError.Code)
-		fmt.Println(customError.Text)
-	}
+	badFunc()
 }
