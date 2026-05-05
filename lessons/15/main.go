092d2580e894d85eb2cdcddd22d5dadc6ba770c9
Ackhapocha <prihodko@bmstu.ru>
2026-05-05 21:06:06 +0300

    Классная работа. Ошибки.

--- lessons/15/main.go
+++ lessons/15/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 var errInF3 = fmt.Errorf("I'm error in f3")
+var errInF2 = fmt.Errorf("Error while calling f3: %w", errInF3)
 
 func f1() error {
 	err := f2()
@@ -17,7 +18,7 @@ func f1() error {
 func f2() error {
 	err := f3() //
 	if err != nil {
-		return fmt.Errorf("Error while calling f3: %w", err)
+		return errInF2
 	}
 	return nil
 }
@@ -27,7 +28,7 @@ func f3() error {
 func main() {
 	err := f1()
 
-	if errors.Is(err, errInF3) {
+	if errors.Is(err, errInF2) {
 		fmt.Println("Yes, it is")
 	}
 }
