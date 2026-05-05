ee9e2520c488719d18a034f5f914e8e53ff5ed76
Ackhapocha <prihodko@bmstu.ru>
2026-05-05 19:31:28 +0300

    Классная работа. Ошибки.

--- lessons/15/main.go
+++ lessons/15/main.go
@@ -5,14 +5,34 @@ import (
 	"os"
 )
 
-func main() {
-	files, err := os.ReadDir(".")
+func WriteToFile(name string) error {
+	f, err := os.Create(name)
 	if err != nil {
-		fmt.Println("Can't read directory =(\n", err)
+		fmt.Println(err)
+		return err
+	}
+	_, err = fmt.Fprintln(f, "Lesson 15 golang")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return err
+	}
+	//some logic
+	_, err = fmt.Fprintln(f, "Finished Writing")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return err
+	}
+	f.Close()
+	return nil
+}
+
+func main() {
+	err := WriteToFile("test.txt")
+	if err != nil {
+		fmt.Println(err)
 		return
-		//os.Exit(1)
 	}
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
+	fmt.Println("All is OK!")
 }
