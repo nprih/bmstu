5a257a38a432e2e6f4a9370c5c4dcbc9d8dc119b
Ackhapocha <prihodko@bmstu.ru>
2026-05-05 21:41:47 +0300

    Классная работа. Ошибки.

--- lessons/15/main.go
+++ lessons/15/main.go
@@ -2,16 +2,6 @@ package main
 
 import "fmt"
 
-func badFunc() {
-	defer func() {
-		r := recover()
-		if r != nil {
-			fmt.Println("It was panic, but i am stronger")
-			fmt.Println("panic was: ", r)
-		}
-	}()
-	panic("I am panic Dmitry, HI")
-}
 func main() {
 	defer func() {
 		r := recover()
