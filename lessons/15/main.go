3a5e81d9b9839a281b86f4cbe497bd15d253e4c6
Ackhapocha <prihodko@bmstu.ru>
2026-05-05 20:22:26 +0300

    Классная работа. Ошибки. Создание ошибок.

--- lessons/15/main.go
+++ lessons/15/main.go
@@ -1,48 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
-	"os"
 )
 
-type NewWriter struct {
-	w   io.Writer
-	err error
-}
-
-func (nw *NewWriter) WriteNewLine(line string) {
-	if nw.err != nil {
-		return
-	}
-	_, nw.err = fmt.Fprintln(nw.w, line)
-}
-func WriteToFile(name string) error {
-	file, err := os.Create(name)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer file.Close()
-	nw := NewWriter{
-		w: file,
-	}
-	nw.WriteNewLine("lesson 15 go")
-	nw.WriteNewLine("hello")
-	nw.WriteNewLine("This")
-	nw.WriteNewLine("is")
-	nw.WriteNewLine("best")
-	nw.WriteNewLine("BMSTU")
-	nw.WriteNewLine("Course")
-	nw.WriteNewLine("that's all")
-	return nw.err
-}
-
 func main() {
-	err := WriteToFile("test.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("All is OK!")
+	err := fmt.Errorf("Some %s", "Error")
+	err = errors.New(fmt.Sprintf("Another %s", "Error"))
 }
