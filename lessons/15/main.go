3e9eaa4ab21943e2a79d032d0236fced0f25275b
Ackhapocha <prihodko@bmstu.ru>
2026-05-05 20:30:49 +0300

    Классная работа. Ошибки. Создание ошибок.

--- lessons/15/main.go
+++ lessons/15/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
+type newError struct{}
+
+func (newerr newError) Error() string {
+	return "This is new Error"
+}
+
+func someFunc() error {
+	var NextGenError newError
+	return NextGenError
+}
+
 func main() {
-	err := fmt.Errorf("Some %s", "Error")
-	err = errors.New(fmt.Sprintf("Another %s", "Error"))
+	err := fmt.Errorf("Some %s", "Error") // error - interface -> Error()
+	//err = errors.New(fmt.Sprintf("Another %s", "Error"))
+	fmt.Println(someFunc())
+
 }
