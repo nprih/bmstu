39765b67ddf5fad1b8457803820192e57a62ec1f
Ackhapocha <prihodko@bmstu.ru>
2026-05-06 23:21:13 +0300

    Решение домашнего задания 1.11 Задача №2 решена.

--- home-works/1_11/02.go
+++ home-works/1_11/02.go
@@ -3,34 +3,40 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
+
+const countNum = 10
+
+func generator(ch chan int, wg *sync.WaitGroup) {
+	for i := 1; i <= countNum; i++ {
+		ch <- i
+	}
+	close(ch)
+	wg.Wait()
+	fmt.Printf("\nКанал закрыт.\n\n")
+}
+
+func printer(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		num, opened := <-ch
+		if opened == false {
+			return
+		}
+		fmt.Printf("Прочитано из канала: %d\n", num)
+	}
+}
+
+func run(ch chan int, wg *sync.WaitGroup) {
+	go generator(ch, wg)
+	go printer(ch, wg)
+}
 
 func main() {
 	ch := make(chan int)
-
 	var wg sync.WaitGroup
 	wg.Add(1)
-
-	go func() {
-		for i := 1; i <= 10; i++ {
-			ch <- i
-		}
-		close(ch)
-		fmt.Println("Канал закрыт")
-	}()
-
-	go func() {
-		defer wg.Done()
-		time.Sleep(time.Duration(30) * time.Second)
-		for {
-			num, opened := <-ch
-			if opened == false {
-				return
-			}
-			fmt.Println("Получено из канала: ", num)
-		}
-	}()
-
+	run(ch, &wg)
 	wg.Wait()
+	fmt.Println("Выполнение программы завершено.")
 }
