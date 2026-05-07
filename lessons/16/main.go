39af107bfdaeb4bb78386accb80c102543e41f19
Ackhapocha <prihodko@bmstu.ru>
2026-05-07 19:53:54 +0300

    Классная работа. Роутинг.

---
+++ lessons/16/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Index page"))
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About page"))
+}
+func contactsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Contacts page"))
+}
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/contacts", contactsHandler)
+
+	log.Println("Server starting...")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
