a65bf285e28ec4c3e6f9bba31f2ebab53ec0f515
Ackhapocha <prihodko@bmstu.ru>
2026-05-14 20:29:40 +0300

    Классная работа. Профилировщик.

--- lessons/17/main.go
+++ lessons/17/main.go
@@ -9,13 +9,12 @@ const sliceSize = 10_000_000
 
 func foo() {
 	for {
-		var s []int
+		var s = make([]int, 0, sliceSize)
 		for i := 0; i < sliceSize; i++ {
 			s = append(s, i)
 		}
 	}
 }
-
 func main() {
 	go foo()
 	http.ListenAndServe(":8080", nil)
---
+++ lessons/17/pprof.md
@@ -0,0 +1,1 @@
+go tool pprof -http=":9090" -seconds=30 http://localhost:8080/debug/pprof/profile
