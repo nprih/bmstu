a61673af32824cb29eea9b7cdbeeef233573d980
Ackhapocha <prihodko@bmstu.ru>
2026-05-19 20:03:39 +0300

    Классная работа. REST

--- lessons/19/main.go
+++ lessons/19/main.go
@@ -1,8 +1,22 @@
 package main
 
 /*
-Cache-Control: no-store
-Cache-Control: public, max-age=3600
+1 Идентификация через URI
+	/users/67
+	/users/
+	/users/67/contacts
+
+2 Манипуляция ресурсами через представления
+	JSON, XML, HTML и т.п.
+
+3 Все сообщения самодокументируемые
+	Content-type
+	HTTP-Method
+
+	для результата - актуальный статус код
+
+4 Многоуровневый доступ
+	client -> [Load balancer] -> [API gateway] -> [Service]
 */
 
 func main() {
