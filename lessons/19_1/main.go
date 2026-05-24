95e85ef89125c7270f1afb6ae365ffeb24550d7e
Ackhapocha <prihodko@bmstu.ru>
2026-05-24 10:09:42 +0300

    Классная работа. SQLite

--- lessons/19_1/main.go
+++ lessons/19_1/main.go
@@ -6,6 +6,13 @@ import (
 )
 import _ "modernc.org/sqlite"
 
+type Phone struct {
+	Id     int
+	Vendor string
+	Model  string
+	Price  int
+}
+
 func main() {
 	db, err := sql.Open("sqlite", "phones")
 	if err != nil {
@@ -14,4 +21,27 @@ func main() {
 	}
 	defer db.Close()
 
+	_, err = db.Exec("INSERT INTO phones (vendor, model, price) VALUES (?, ?, ?)", "Apple", "iPhone 12", 7000)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM phones")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+	phones := []Phone{}
+	for rows.Next() {
+		ph := Phone{}
+		err := rows.Scan(&ph.Id, &ph.Vendor, &ph.Model, &ph.Price)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		phones = append(phones, ph)
+	}
+	fmt.Println(phones)
 }
