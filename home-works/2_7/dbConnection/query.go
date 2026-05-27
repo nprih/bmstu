9050bf72b27719ec02da973084080c792425352a
Ackhapocha <prihodko@bmstu.ru>
2026-05-28 01:42:51 +0300

    Решение домашнего задания 2.7. Рефакторинг.

--- home-works/2_7/dbConnection/query.go
+++ home-works/2_7/dbConnection/query.go
@@ -1,8 +1,11 @@
 package dbConnection
 
 import (
+	"2_7/functions"
 	"database/sql"
+	"fmt"
 	"log"
+	"strings"
 )
 
 func ClearDb(db *sql.DB) {
@@ -11,6 +14,31 @@ func ClearDb(db *sql.DB) {
 		log.Fatal(err)
 	}
 	_, err = db.Exec("DELETE FROM sqlite_sequence WHERE name = 'users'")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func AddDefaultUsers() {
+	err, db := DbConnection()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	ClearDb(db)
+
+	var placeholders []string
+	var args []interface{}
+	for _, user := range functions.UsersHashingPass() {
+		placeholders = append(placeholders, "(?, ?)")
+		args = append(args, user.Email, user.Password)
+	}
+
+	query := `INSERT INTO users (email, password) VALUES ` + strings.Join(placeholders, ", ")
+
+	_, err = db.Exec(query, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
--- home-works/2_7/main.go
+++ home-works/2_7/main.go
@@ -3,41 +3,13 @@ package main
 import (
 	"2_7/argon2"
 	"2_7/dbConnection"
-	"2_7/functions"
 	"2_7/input"
 	"2_7/models"
 	"errors"
 	"fmt"
-	"log"
-	"strings"
 
 	_ "modernc.org/sqlite"
 )
-
-func addDefaultUsers() {
-	err, db := dbConnection.DbConnection()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
-
-	dbConnection.ClearDb(db)
-
-	var placeholders []string
-	var args []interface{}
-	for _, user := range functions.UsersHashingPass() {
-		placeholders = append(placeholders, "(?, ?)")
-		args = append(args, user.Email, user.Password)
-	}
-
-	query := `INSERT INTO users (email, password) VALUES ` + strings.Join(placeholders, ", ")
-
-	_, err = db.Exec(query, args...)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func auth(pare input.Pare) {
 	user, err := findUserByLogin(pare.Login)
@@ -87,7 +59,7 @@ func findUserByLogin(login string) (models.User, error) {
 }
 
 func main() {
-	addDefaultUsers()
+	dbConnection.AddDefaultUsers()
 
 	err, pare := input.InputAuth()
 	if err != nil {
