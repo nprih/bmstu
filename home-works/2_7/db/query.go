e83a2879f86c0f8e45a3fa9ecee022b142aabfb6
Ackhapocha <prihodko@bmstu.ru>
2026-05-28 01:48:00 +0300

    Решение домашнего задания 2.7. Рефакторинг.

--- home-works/2_7/db/query.go
+++ home-works/2_7/db/query.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"2_7/functions"
+	"2_7/models"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
@@ -42,4 +44,36 @@ func AddDefaultUsers() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func FindUserByLogin(login string) (models.User, error) {
+	err, db := DbConnection()
+	if err != nil {
+		fmt.Println(err)
+		return models.User{}, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users WHERE email = $1", login)
+	if err != nil {
+		fmt.Println(err)
+		return models.User{}, err
+	}
+	defer rows.Close()
+
+	users := []models.User{}
+	for rows.Next() {
+		user := models.User{}
+		err := rows.Scan(&user.Id, &user.Email, &user.Password)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		users = append(users, user)
+	}
+	if len(users) != 0 {
+		return users[0], nil
+	}
+
+	return models.User{}, errors.New(fmt.Sprintf("\nПользователь \"%s\" не найден", login))
 }
--- home-works/2_7/main.go
+++ home-works/2_7/main.go
@@ -4,15 +4,13 @@ import (
 	"2_7/argon2"
 	"2_7/db"
 	"2_7/input"
-	"2_7/models"
-	"errors"
 	"fmt"
 
 	_ "modernc.org/sqlite"
 )
 
 func auth(pare input.Pare) {
-	user, err := findUserByLogin(pare.Login)
+	user, err := db.FindUserByLogin(pare.Login)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -24,38 +22,6 @@ func auth(pare input.Pare) {
 		return
 	}
 	fmt.Println("Пароль верный")
-}
-
-func findUserByLogin(login string) (models.User, error) {
-	err, db := db.DbConnection()
-	if err != nil {
-		fmt.Println(err)
-		return models.User{}, err
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT * FROM users WHERE email = $1", login)
-	if err != nil {
-		fmt.Println(err)
-		return models.User{}, err
-	}
-	defer rows.Close()
-
-	users := []models.User{}
-	for rows.Next() {
-		user := models.User{}
-		err := rows.Scan(&user.Id, &user.Email, &user.Password)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		users = append(users, user)
-	}
-	if len(users) != 0 {
-		return users[0], nil
-	}
-
-	return models.User{}, errors.New(fmt.Sprintf("\nПользователь \"%s\" не найден", login))
 }
 
 func main() {
