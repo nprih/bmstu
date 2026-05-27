21fc680fe334731018875f8793db757811528c55
Ackhapocha <prihodko@bmstu.ru>
2026-05-28 01:52:01 +0300

    Решение домашнего задания 2.7. Рефакторинг.

--- home-works/2_7/main.go
+++ home-works/2_7/main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"2_7/argon2"
+	"2_7/auth"
 	"2_7/db"
 	"2_7/input"
 	"fmt"
 
 	_ "modernc.org/sqlite"
 )
-
-func auth(pare input.Pare) {
-	user, err := db.FindUserByLogin(pare.Login)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	ok, _ := argon2.CheckPasswordArgon2(pare.Password, user.Password)
-	if !ok {
-		fmt.Println("Пароль неверный")
-		return
-	}
-	fmt.Println("Пароль верный")
-}
 
 func main() {
 	db.AddDefaultUsers()
@@ -33,5 +18,5 @@ func main() {
 		return
 	}
 
-	auth(pare)
+	auth.Auth(pare)
 }
