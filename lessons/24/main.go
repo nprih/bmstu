43d33ee7bc73ea82755a537f75bf3c5f3f19ac1c
Ackhapocha <prihodko@bmstu.ru>
2026-06-07 12:18:18 +0300

    Классная работа. Gin

--- lessons/24/main.go
+++ lessons/24/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 type User struct {
@@ -17,10 +18,12 @@ type User struct {
 func indexHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello from gin")
 }
+
 func statusHandler(c *gin.Context) {
 	query := c.DefaultQuery("id", "no service")
 	c.String(http.StatusOK, "Status for %s: ok", query)
 }
+
 func getUserById(c *gin.Context) {
 	id := c.Param("id")
 	IntId, err := strconv.Atoi(id)
@@ -34,6 +37,7 @@ func getUserById(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, user)
 }
+
 func createNewUser(c *gin.Context) {
 	var newUser User
 	if err := c.ShouldBindJSON(&newUser); err != nil {
@@ -47,26 +51,32 @@ func createNewUser(c *gin.Context) {
 		"user":    newUser,
 	})
 }
+
 func AuthMiddeware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		fmt.Println("New Auth Request:", c.Request.Method, c.Request.URL.Path)
 		c.Next()
 	}
 }
+
 func AdminMiddeware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		fmt.Println("ONLY FOR ADMIN!!")
 		c.Next()
 	}
 }
+
 func main() {
 	r := gin.Default()
 	r.GET("/", indexHandler)
+
 	r.Use(AuthMiddeware())
 	r.GET("/user/:id", getUserById)
 	r.POST("/user", createNewUser)
+
 	r.Use(AdminMiddeware())
 	r.GET("/status", statusHandler)
+
 	if err := r.Run(":8080"); err != nil {
 		log.Println(err)
 		return
