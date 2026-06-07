92085232119a8c9b3a83c97a67af593edbae8b8b
Ackhapocha <prihodko@bmstu.ru>
2026-06-07 11:33:21 +0300

    Классная работа. Gin

--- lessons/24/main.go
+++ lessons/24/main.go
@@ -17,10 +17,6 @@ func indexHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello from gin")
 }
 
-func greetUserHandler(c *gin.Context) {
-	name := c.Param("name")
-	c.String(http.StatusOK, "Hello, %s", name)
-}
 func statusHandler(c *gin.Context) {
 	query := c.DefaultQuery("id", "no service")
 	c.String(http.StatusOK, "Status %s: ok", query)
@@ -43,7 +39,6 @@ func getUserById(c *gin.Context) {
 func main() {
 	r := gin.Default()
 	r.GET("/", indexHandler)
-	//r.GET("/user/:name", greetUserHandler)
 	r.GET("/user/: id", getUserById)
 	r.GET("/status", statusHandler)
 
