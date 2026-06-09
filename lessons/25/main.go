eb2059212de706437f0cfb4d2ffcda8aa5a68d87
Ackhapocha <prihodko@bmstu.ru>
2026-06-09 20:04:39 +0300

    Классная работа. Хеширование

--- lessons/25/main.go
+++ lessons/25/main.go
@@ -1,35 +1,25 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
+	"crypto/aes"
 	"fmt"
+	"log"
 )
 
-func GenerateHMAC(key, message []byte) string {
-	h := hmac.New(sha256.New, key)
-	h.Write(message)
-	return hex.EncodeToString(h.Sum(nil))
-}
-func VerifyHMAC(key, message []byte, signature string) bool {
-	key = []byte("secret-key-123")
-	h := hmac.New(sha256.New, key)
-	h.Write(message)
-	expectedSignature := hex.EncodeToString(h.Sum(nil))
-	expectedBytes, expErr := hex.DecodeString(expectedSignature)
-	receivedBytes, recErr := hex.DecodeString(signature)
-	if expErr != nil || recErr != nil {
-		return false
-	}
-	return hmac.Equal(expectedBytes, receivedBytes)
-}
+var key = []byte("examplekey123456")
+
 func main() {
-	key := []byte("secret-key-123")
-	message := []byte("Hello with HMAC!")
-	signature := GenerateHMAC(key, message)
-	fmt.Printf("Message: %s\n", string(message))
-	fmt.Printf("HMAC signature: %s\n\n", signature)
-	isValid := VerifyHMAC(key, message, signature)
-	fmt.Printf("IsValid: %v\n", isValid)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	myMessage := []byte("Hello my friends!")
+	ciphertext := make([]byte, len(myMessage))
+	block.Encrypt(ciphertext, myMessage)
+	fmt.Printf("%x\n", ciphertext)
+
+	clearMessage := make([]byte, len(ciphertext))
+	block.Decrypt(clearMessage, ciphertext)
+	fmt.Printf("%s\n", clearMessage)
 }
