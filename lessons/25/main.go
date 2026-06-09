93cdea9187444ce318000ba289cb6cd43504932c
Ackhapocha <prihodko@bmstu.ru>
2026-06-09 20:56:41 +0300

    Классная работа. Шифрование

--- lessons/25/main.go
+++ lessons/25/main.go
@@ -1,47 +1,14 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"fmt"
 	"log"
+	"net/http"
 )
 
 func main() {
-	//Generate keys
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	publicKey := &privateKey.PublicKey
-	message := []byte("Hello my friends!")
-	//create chipher
-	ciphertext, err := rsa.EncryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		publicKey,
-		message,
-		nil,
-	)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Printf("Message: %s\n", message)
-	fmt.Printf("Cipher: %x\n", ciphertext)
-	//clear text
-	clearText, err := rsa.DecryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		privateKey,
-		ciphertext,
-		nil,
-	)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Printf("Message: %s\n", clearText)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("INDEX PAGE, HELLO WITH RSA!!!)))"))
+	})
+	log.Println("Listening...")
+	http.ListenAndServeTLS(":8085", "rootCA.pem", "rootCA.key", nil)
 }
