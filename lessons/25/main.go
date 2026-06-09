9caff565e7b06f57a3e365b940df9ad0b8186cf5
Ackhapocha <prihodko@bmstu.ru>
2026-06-09 20:38:15 +0300

    Классная работа. Шифрование

--- lessons/25/main.go
+++ lessons/25/main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"crypto/aes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
 	"fmt"
 	"log"
 )
 
-var key = []byte("examplekey123456")
-
 func main() {
-	block, err := aes.NewCipher(key)
+	//Generate keys
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	myMessage := []byte("Hello my friends!")
-	ciphertext := make([]byte, len(myMessage))
-	block.Encrypt(ciphertext, myMessage)
-	fmt.Printf("%x\n", ciphertext)
-
-	clearMessage := make([]byte, len(ciphertext))
-	block.Decrypt(clearMessage, ciphertext)
-	fmt.Printf("%s\n", clearMessage)
+	publicKey := &privateKey.PublicKey
+	message := []byte("Hello my friends!")
+	//create chipher
+	ciphertext, err := rsa.EncryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		publicKey,
+		message,
+		nil,
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("Message: %s\n", message)
+	fmt.Printf("Cipher: %x\n", ciphertext)
+	//clear text
+	clearText, err := rsa.DecryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		privateKey,
+		ciphertext,
+		nil,
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("Message: %s\n", clearText)
 }
