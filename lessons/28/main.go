362a1d5c14e0e313b859450fe9b3ee96f699ec20
Ackhapocha <prihodko@bmstu.ru>
2026-06-18 21:38:14 +0300

    Классная работа. Docker.

--- lessons/28/main.go
+++ lessons/28/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	version := os.Getenv("VERSION")
+	if version == "" {
+		version = "1.0.0"
+	}
+	fmt.Fprintf(w, "Heloo from docker (version: %s)\n", version)
+}
+
+func main() {
+	http.HandleFunc("/status", IndexHandler)
+	log.Println("Server starting...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Println(err)
+		return
+	}
 }
